110c59c62e7b4b920b3adea406542804389ddfde
caroline short <carolineshort94@gmail.com>
2025-07-21 19:11:04 -0700

    home page started

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,17 +1,40 @@
-import { userAuth } from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../context/AuthProvider";
+import Header from "./Header"
+import "./style/Home.css"
 
 
-export default function Home() {
-    const { user, logout } = userAuth();
-    const navigate = useNavigate();
+export default function HomePage() {
+    const [requests, setRequests] = useState([])
 
-    const handleLogin = () => navigate("/login")
-    const handleSignup = () => navigate("/signup")
+    useEffect(() => {
+        fetch("http://localhost:8000/requests?user_id=1")
+            .then((res) => res.json())
+            .then((data) => setRequests(data))
+            .catch((err) => console.error("Failed to fetch requests", err))
+    }, [])
 
     return (
-        <div>
+        <>
+            <Header />
+            <main className="home-page">
+                <aside className="sidebar">
+                    <h2>What do you need?</h2>
+                    <p>Tell us what product or service you're
+                        looking for!
+                    </p>
+                    <Link to='/new-request' className='button green'>
+                        Post a New Request</Link>
 
-        </div>
+                    <h3>Are you a Seller?</h3>
+                    <p>Find matching requests and submit your offers.</p>
+                    <Link to='/requests' className='button purple'>
+                        View Open Requests</Link>
+                </aside>
+
+
+            </main>
+
+        </>
     )
 }
